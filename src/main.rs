883216f9797591637ce232fc5ef888177d1720c8
Pierce Thompson <insprill@gmail.com>
2022-11-29 16:07:00 -0500

    Add flag to specify worker count, cleanup error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{
     env,
+    error::Error,
     fs::{self, File},
     io::Write,
     process::{Command, Stdio},
@@ -12,6 +13,7 @@ use log::LevelFilter;
 use clap::{command, Parser};
 use platform_dirs::AppDirs;
 use simplelog::{ColorChoice, CombinedLogger, Config, TermLogger, TerminalMode};
+use tokio::runtime::Builder;
 
 pub mod adoptium;
 pub mod mojang;
@@ -22,31 +24,39 @@ pub mod spigot;
 struct Args {
     /// What versions to run BuildTools for
     versions: Vec<String>,
+
+    /// How many workers are used to perform tasks. This includes downloading Java, and running BuildTools.
+    #[arg(short, long)]
+    workers: Option<usize>,
 }
 
-#[tokio::main]
-async fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     CombinedLogger::init(vec![TermLogger::new(
         LevelFilter::Info,
         Config::default(),
         TerminalMode::Mixed,
         ColorChoice::Auto,
-    )])
-    .unwrap();
+    )])?;
 
     let args = Args::parse();
 
-    let manifests = mojang::map_version_manifests(args.versions)
-        .await
-        .unwrap_or_else(|err| {
-            panic!("Failed to fetch version manifest: {:?}", err);
-        });
+    let mut runtime = Builder::new_multi_thread();
+    runtime.enable_time();
+    runtime.enable_io();
 
-    let packages = mojang::fetch_packages(manifests.clone())
-        .await
-        .unwrap_or_else(|err| {
-            panic!("Failed to fetch version manifest: {:?}", err);
-        });
+    if let Some(w) = args.workers {
+        if w > 0 {
+            runtime.worker_threads(w);
+        }
+    }
+
+    runtime.build()?.block_on(run(args))
+}
+
+async fn run(args: Args) -> Result<(), Box<dyn Error>> {
+    let manifests = mojang::map_version_manifests(args.versions).await?;
+
+    let packages = mojang::fetch_packages(manifests.clone()).await?;
 
     let java_versions: Vec<u8> = packages
         .iter()
@@ -55,9 +65,7 @@ async fn main() {
         .unique()
         .collect();
 
-    let java_releases = adoptium::get_releases().await.unwrap_or_else(|err| {
-        panic!("Failed to fetch available Java versions: {:?}", err);
-    });
+    let java_releases = adoptium::get_releases().await?;
 
     if let Some(unavail) = java_versions
         .iter()
@@ -72,49 +80,30 @@ async fn main() {
 
     let java_dir = cache_dir.join("java");
 
-    adoptium::try_download_versions(java_versions, &java_dir)
-        .await
-        .unwrap_or_else(|err| {
-            panic!("Failed to download Java: {:?}", err);
-        });
+    adoptium::try_download_versions(java_versions, &java_dir).await?;
 
-    let buildtools_jar_data = spigot::download_buildtools().await.unwrap_or_else(|err| {
-        panic!("Failed to download buildtools: {:?}", err);
-    });
+    let buildtools_jar_data = spigot::download_buildtools().await?;
 
     let bt_file_dir = cache_dir.join("buildtools.jar");
-    let mut bt_file = File::create(&bt_file_dir).unwrap_or_else(|err| {
-        panic!("Failed to create buildtools file: {:?}", err);
-    });
-    bt_file.set_len(0).unwrap_or_else(|err| {
-        panic!("Failed to remove old BuildTools jar: {:?}", err);
-    });
-    bt_file
-        .write_all(&buildtools_jar_data)
-        .unwrap_or_else(|err| {
-            panic!("Failed to write to BuildTools jar: {:?}", err);
-        });
+
+    let mut bt_file = File::create(&bt_file_dir)?;
+    bt_file.set_len(0)?;
+    bt_file.write_all(&buildtools_jar_data)?;
 
     let bt_tmp_dir = env::temp_dir().join("buildtools");
 
     let mut handles = Vec::with_capacity(packages.len());
     for package in packages {
         let bt_dir = bt_tmp_dir.join(&package.id);
-        fs::create_dir_all(&bt_dir).unwrap_or_else(|err| {
-            panic!("Failed to create BuildTools dir: {:?}", err);
-        });
+        fs::create_dir_all(&bt_dir)?;
+
         let java_dir = java_dir.clone();
         let bt_file_dir = bt_file_dir.clone();
+
+        let install_dir =
+            adoptium::get_java_install(package.java_version.major_version, &java_dir).await?;
+
         handles.push(tokio::spawn(async move {
-            let install_dir =
-                adoptium::get_java_install(package.java_version.major_version, &java_dir)
-                    .await
-                    .unwrap_or_else(|err| {
-                        panic!(
-                            "Failed to find Java {} install: {:?}",
-                            package.java_version.major_version, err
-                        );
-                    });
             Command::new(install_dir.to_string_lossy().to_string())
                 .arg("-jar")
                 .arg(&bt_file_dir.to_string_lossy().to_string())
@@ -129,5 +118,7 @@ async fn main() {
     }
     future::join_all(handles).await;
 
-    fs::remove_dir_all(bt_tmp_dir).expect("Failed to remove temp directory!");
+    fs::remove_dir_all(bt_tmp_dir)?;
+
+    Ok(())
 }

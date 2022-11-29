d59d8a5efb015fcb7dfe88c5eff0f2b17f177a66
Pierce Thompson <insprill@gmail.com>
2022-11-29 03:42:59 -0500

    Set custom user agent when downloading BuildTools

--- src/spigot.rs
+++ src/spigot.rs
@@ -1,9 +1,12 @@
 use log::info;
-use reqwest::{self};
+use reqwest::{self, Client};
 
 pub async fn download_buildtools() -> Result<Vec<u8>, reqwest::Error> {
     info!("Downloading the latest BuildTools release");
-    Ok(reqwest::get("https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar")
+    Ok(Client::new()
+    .get("https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar")
+    .header("User-Agent", env!("CARGO_PKG_NAME"))
+    .send()
     .await?
     .bytes()
     .await?.to_vec())

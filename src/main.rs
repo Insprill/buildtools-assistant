0e1b0eb30c5ebc5a67518e0c66ce69c0c2a02185
Pierce Thompson <insprill@gmail.com>
2022-11-29 21:47:24 -0500

    Default workers to a quarter the core count

--- src/main.rs
+++ src/main.rs
@@ -55,7 +55,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     sys.refresh_memory();
 
     let args = Args::parse();
-    let worker_count = args.workers.unwrap_or_else(|| max(1, sys.cpus().len() / 2));
+    let worker_count = args.workers.unwrap_or_else(|| max(1, sys.cpus().len() / 4));
 
     let runtime = Builder::new_multi_thread()
         .worker_threads(worker_count)
